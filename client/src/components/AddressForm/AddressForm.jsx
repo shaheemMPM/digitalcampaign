b9b366ede68f3c1fa1b512342ddbbc03ccf825b8
shaheemMPM <shaheem.mpm@gmail.com>
2022-06-10 09:54:27 +0530

    Adds loader

--- client/src/components/AddressForm/AddressForm.jsx
+++ client/src/components/AddressForm/AddressForm.jsx
@@ -7,8 +7,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Snackbar from '@mui/material/Snackbar';
-import Alert from '@mui/material/Alert';
+import CircularProgress from '@mui/material/CircularProgress';
 import Modal from '@mui/material/Modal';
 import { makeStyles } from '@mui/styles';
 import Swal from 'sweetalert2';
@@ -161,6 +160,7 @@ export default function AddressForm() {
 	const [imgSrc, setImgSrc] = useState('');
 	const [crop, setCrop] = useState();
 	const [completedCrop, setCompletedCrop] = useState();
+	const [isLoading, setIsLoading] = useState(false);
 
 	const onSelectFile = (e) => {
 		if (e.target.files && e.target.files.length > 0) {
@@ -187,6 +187,21 @@ export default function AddressForm() {
 	const handleBatchChange = (event) => {
 		setBatch(event.target.value);
 		setOthBatch(event.target.value);
+	};
+
+	const resetAll = () => {
+		setSchool('select');
+		setBatch('select');
+		setFullName('');
+		setSslcRegNo('');
+		setMobile('');
+		setOthSchool('');
+		setOthBatch('');
+		setCropModalOpen(false);
+		setImgSrc('');
+		setCrop(undefined);
+		setCompletedCrop(undefined);
+		setIsLoading(false);
 	};
 
 	const handleSubmit = () => {
@@ -218,6 +233,8 @@ export default function AddressForm() {
 			return;
 		}
 
+		setIsLoading(true);
+
 		// Poster download
 		const canvas = posterCanvasRef.current;
 		const context = canvas.getContext('2d');
@@ -232,7 +249,6 @@ export default function AddressForm() {
 		const link = document.createElement('a');
 		link.download = 'insight_poster.png';
 		link.href = canvas.toDataURL('image/png');
-		link.click();
 
 		// Momento upload
 		const mCanvas = momentoCanvasRef.current;
@@ -258,12 +274,13 @@ export default function AddressForm() {
 			})
 			.then((response) => {
 				console.log(response);
+				link.click();
 				Swal.fire({
 					icon: 'success',
 					title: 'Success',
 					text: `Successfully uploaded form! thank you`,
 				}).then(() => {
-					window.location.reload();
+					resetAll();
 				});
 			})
 			.catch((error) => {
@@ -271,6 +288,8 @@ export default function AddressForm() {
 					icon: 'error',
 					title: 'Oops...',
 					text: `Sorry form upload failed, try again!\n\n${error.message}`,
+				}).then(() => {
+					setIsLoading(false);
 				});
 				console.log(error);
 			});
@@ -283,236 +302,244 @@ export default function AddressForm() {
 
 	return (
 		<React.Fragment>
-			<Typography variant='h6' gutterBottom>
-				Fill the form
-			</Typography>
-			<Grid container spacing={3}>
-				<Grid item xs={12}>
-					<TextField
-						required
-						id='fullName'
-						name='fullName'
-						label='Full name'
-						fullWidth
-						variant='standard'
-						value={fullName}
-						onChange={(e) => {
-							setFullName(e.target.value);
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12}>
-					<TextField
-						required
-						id='regNo'
-						name='regNo'
-						label='SSLC Registration Number'
-						fullWidth
-						variant='standard'
-						value={sslcRegNo}
-						onChange={(e) => {
-							setSslcRegNo(e.target.value);
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12}>
-					<TextField
-						required
-						id='mobile'
-						name='mobile'
-						type='tel'
-						label='Mobile Number'
-						fullWidth
-						variant='standard'
-						InputProps={{
-							startAdornment: (
-								<InputAdornment position='start'>+91</InputAdornment>
-							),
-						}}
-						value={mobile}
-						onChange={(e) => {
-							setMobile(e.target.value);
-						}}
-					/>
-				</Grid>
-				<Grid item xs={12}>
-					<TextField
-						required
-						id='school'
-						select
-						label='School Name'
-						value={school}
-						onChange={handleSchoolChange}
-						fullWidth
-						helperText='Please select your school name'
-					>
-						{schoolNames.map((schoolName) => (
-							<MenuItem key={schoolName.value} value={schoolName.value}>
-								{schoolName.label}
-							</MenuItem>
-						))}
-					</TextField>
-				</Grid>
-				{school === 'other' ? (
-					<Grid item xs={12}>
-						<TextField
-							required
-							id='othSchool'
-							name='othSchool'
-							label='Specify School Name'
-							fullWidth
-							variant='standard'
-							value={othSchool}
-							onChange={(e) => {
-								setOthSchool(e.target.value);
-							}}
-						/>
-					</Grid>
-				) : null}
-				<Grid item xs={12}>
-					<TextField
-						required
-						id='batch'
-						select
-						label='Batch Name'
-						value={batch}
-						onChange={handleBatchChange}
-						fullWidth
-						helperText='Please select your batch name'
-					>
-						{batchNames.map((batchName) => (
-							<MenuItem key={batchName.value} value={batchName.value}>
-								{batchName.label}
-							</MenuItem>
-						))}
-					</TextField>
-				</Grid>
-				{batch === 'other' ? (
-					<Grid item xs={12}>
-						<TextField
-							required
-							id='othBatch'
-							name='othBatch'
-							label='Specify Batch Name'
-							fullWidth
-							variant='standard'
-							value={othBatch}
-							onChange={(e) => {
-								setOthBatch(e.target.value);
-							}}
-						/>
-					</Grid>
-				) : null}
-				<Grid item xs={12}>
-					<Button variant='contained' color='warning' component='label'>
-						Upload Image*
-						<input
-							type='file'
-							onChange={onSelectFile}
-							accept='image/*'
-							hidden
-						/>
-					</Button>
-				</Grid>
-				<Grid item xs={12}>
-					<div>
-						{Boolean(completedCrop) && (
-							<canvas
-								ref={cropCanvasRef}
-								style={{
-									display: cropModalOpen ? 'none' : 'block',
-									objectFit: 'contain',
-									width: '150px',
-									height: '150px',
+			{isLoading ? (
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<CircularProgress />
+				</div>
+			) : (
+				<React.Fragment>
+					<Typography variant='h6' gutterBottom>
+						Fill the form
+					</Typography>
+					<Grid container spacing={3}>
+						<Grid item xs={12}>
+							<TextField
+								required
+								id='fullName'
+								name='fullName'
+								label='Full name'
+								fullWidth
+								variant='standard'
+								value={fullName}
+								onChange={(e) => {
+									setFullName(e.target.value);
 								}}
 							/>
-						)}
-					</div>
-				</Grid>
-			</Grid>
-			<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-				<Button
-					variant='contained'
-					onClick={handleSubmit}
-					sx={{ mt: 3, ml: 1 }}
-				>
-					SUBMIT
-				</Button>
-			</Box>
-			<Modal
-				aria-labelledby='transition-modal-title'
-				aria-describedby='transition-modal-description'
-				className={classes.modal}
-				open={cropModalOpen}
-				onClose={(_, reason) => {
-					if (reason && reason === 'backdropClick') return;
-					setCropModalOpen(false);
-				}}
-				disableEscapeKeyDown
-			>
-				<div className={classes.paper}>
-					{Boolean(imgSrc) && (
-						<ReactCrop
-							crop={crop}
-							onChange={(_, percentCrop) => setCrop(percentCrop)}
-							onComplete={(c) => setCompletedCrop(c)}
-							aspect={1}
-						>
-							<img
-								style={{ maxHeight: '70vh' }}
-								ref={imgRef}
-								alt='Crop me'
-								src={imgSrc}
-								onLoad={onImageLoad}
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								id='regNo'
+								name='regNo'
+								label='SSLC Registration Number'
+								fullWidth
+								variant='standard'
+								value={sslcRegNo}
+								onChange={(e) => {
+									setSslcRegNo(e.target.value);
+								}}
 							/>
-						</ReactCrop>
-					)}
-					<Button
-						color='error'
-						variant='contained'
-						onClick={() => {
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								id='mobile'
+								name='mobile'
+								type='tel'
+								label='Mobile Number'
+								fullWidth
+								variant='standard'
+								InputProps={{
+									startAdornment: (
+										<InputAdornment position='start'>+91</InputAdornment>
+									),
+								}}
+								value={mobile}
+								onChange={(e) => {
+									setMobile(e.target.value);
+								}}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								id='school'
+								select
+								label='School Name'
+								value={school}
+								onChange={handleSchoolChange}
+								fullWidth
+								helperText='Please select your school name'
+							>
+								{schoolNames.map((schoolName) => (
+									<MenuItem key={schoolName.value} value={schoolName.value}>
+										{schoolName.label}
+									</MenuItem>
+								))}
+							</TextField>
+						</Grid>
+						{school === 'other' ? (
+							<Grid item xs={12}>
+								<TextField
+									required
+									id='othSchool'
+									name='othSchool'
+									label='Specify School Name'
+									fullWidth
+									variant='standard'
+									value={othSchool}
+									onChange={(e) => {
+										setOthSchool(e.target.value);
+									}}
+								/>
+							</Grid>
+						) : null}
+						<Grid item xs={12}>
+							<TextField
+								required
+								id='batch'
+								select
+								label='Batch Name'
+								value={batch}
+								onChange={handleBatchChange}
+								fullWidth
+								helperText='Please select your batch name'
+							>
+								{batchNames.map((batchName) => (
+									<MenuItem key={batchName.value} value={batchName.value}>
+										{batchName.label}
+									</MenuItem>
+								))}
+							</TextField>
+						</Grid>
+						{batch === 'other' ? (
+							<Grid item xs={12}>
+								<TextField
+									required
+									id='othBatch'
+									name='othBatch'
+									label='Specify Batch Name'
+									fullWidth
+									variant='standard'
+									value={othBatch}
+									onChange={(e) => {
+										setOthBatch(e.target.value);
+									}}
+								/>
+							</Grid>
+						) : null}
+						<Grid item xs={12}>
+							<Button variant='contained' color='warning' component='label'>
+								Upload Image*
+								<input
+									type='file'
+									onChange={onSelectFile}
+									accept='image/*'
+									hidden
+								/>
+							</Button>
+						</Grid>
+						<Grid item xs={12}>
+							<div>
+								{Boolean(completedCrop) && (
+									<canvas
+										ref={cropCanvasRef}
+										style={{
+											display: cropModalOpen ? 'none' : 'block',
+											objectFit: 'contain',
+											width: '150px',
+											height: '150px',
+										}}
+									/>
+								)}
+							</div>
+						</Grid>
+					</Grid>
+					<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+						<Button
+							variant='contained'
+							onClick={handleSubmit}
+							sx={{ mt: 3, ml: 1 }}
+						>
+							SUBMIT
+						</Button>
+					</Box>
+					<Modal
+						aria-labelledby='transition-modal-title'
+						aria-describedby='transition-modal-description'
+						className={classes.modal}
+						open={cropModalOpen}
+						onClose={(_, reason) => {
+							if (reason && reason === 'backdropClick') return;
 							setCropModalOpen(false);
-							setImgSrc('');
-							setCrop(undefined);
-							setCompletedCrop(undefined);
 						}}
-						sx={{ mt: 3, ml: 1 }}
+						disableEscapeKeyDown
 					>
-						CANCEL
-					</Button>
-					<Button
-						variant='contained'
-						onClick={onCropHandler}
-						sx={{ mt: 1, ml: 1 }}
-					>
-						CROP
-					</Button>
-				</div>
-			</Modal>
-			<canvas
-				ref={posterCanvasRef}
-				style={{ display: 'none' }}
-				width='1566'
-				height='1655'
-			/>
-			<canvas
-				ref={momentoCanvasRef}
-				style={{ display: 'none' }}
-				width='1115'
-				height='1655'
-			/>
-			<img
-				src={posterFrameSrc}
-				alt='poster frame'
-				ref={posterFrameImageRef}
-				style={{ display: 'none' }}
-			/>
-			<img
-				src={momentoFrameSrc}
-				alt='momento frame'
-				ref={momentoFrameImageRef}
-				style={{ display: 'none' }}
-			/>
+						<div className={classes.paper}>
+							{Boolean(imgSrc) && (
+								<ReactCrop
+									crop={crop}
+									onChange={(_, percentCrop) => setCrop(percentCrop)}
+									onComplete={(c) => setCompletedCrop(c)}
+									aspect={1}
+								>
+									<img
+										style={{ maxHeight: '70vh' }}
+										ref={imgRef}
+										alt='Crop me'
+										src={imgSrc}
+										onLoad={onImageLoad}
+									/>
+								</ReactCrop>
+							)}
+							<Button
+								color='error'
+								variant='contained'
+								onClick={() => {
+									setCropModalOpen(false);
+									setImgSrc('');
+									setCrop(undefined);
+									setCompletedCrop(undefined);
+								}}
+								sx={{ mt: 3, ml: 1 }}
+							>
+								CANCEL
+							</Button>
+							<Button
+								variant='contained'
+								onClick={onCropHandler}
+								sx={{ mt: 1, ml: 1 }}
+							>
+								CROP
+							</Button>
+						</div>
+					</Modal>
+					<canvas
+						ref={posterCanvasRef}
+						style={{ display: 'none' }}
+						width='1566'
+						height='1655'
+					/>
+					<canvas
+						ref={momentoCanvasRef}
+						style={{ display: 'none' }}
+						width='1115'
+						height='1655'
+					/>
+					<img
+						src={posterFrameSrc}
+						alt='poster frame'
+						ref={posterFrameImageRef}
+						style={{ display: 'none' }}
+					/>
+					<img
+						src={momentoFrameSrc}
+						alt='momento frame'
+						ref={momentoFrameImageRef}
+						style={{ display: 'none' }}
+					/>
+				</React.Fragment>
+			)}
 		</React.Fragment>
 	);
 }
