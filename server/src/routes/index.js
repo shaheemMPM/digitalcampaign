f2759df04edf33dd8b21e2c1cdd28b36c17dc2a1
shaheemMPM <shaheem.mpm@gmail.com>
2022-06-20 15:37:51 +0530

    Adds cleandb endpoint

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -18,36 +18,50 @@ router.get('/', (req, res) => {
 	});
 });
 
-router.get('/extract', async (req, res) => {
-	const users = await User.find();
+// router.get('/extract', async (req, res) => {
+// 	const users = await User.find();
 
-	const newUsers = [];
-	for (let i = 0; i < users.length; i++) {
-		const user = users[i];
-		newUsers.push({
-			sslcRegNo: user.sslcRegNo,
-			fullName: user.fullName,
-			mobile: user.mobile,
-			school: user.school,
-			batch: user.batch,
-			momento: user.momento,
-			time: new Date(user.updatedAt).toLocaleString(),
-		});
-	}
+// 	const newUsers = [];
+// 	for (let i = 0; i < users.length; i++) {
+// 		const user = users[i];
+// 		newUsers.push({
+// 			sslcRegNo: user.sslcRegNo,
+// 			fullName: user.fullName,
+// 			mobile: user.mobile,
+// 			school: user.school,
+// 			batch: user.batch,
+// 			momento: user.momento,
+// 			time: new Date(user.updatedAt).toLocaleString(),
+// 		});
+// 	}
 
-	fs.writeFile('users.json', JSON.stringify(newUsers), 'utf8', (err) => {
-		if (err) {
-			res.status(200).json({
-				message: 'failed',
-				err,
-			});
-		} else {
-			res.status(200).json({
-				message: 'success',
-			});
-		}
-	});
-});
+// 	fs.writeFile('users.json', JSON.stringify(newUsers), 'utf8', (err) => {
+// 		if (err) {
+// 			res.status(200).json({
+// 				message: 'failed',
+// 				err,
+// 			});
+// 		} else {
+// 			res.status(200).json({
+// 				message: 'success',
+// 			});
+// 		}
+// 	});
+// });
+
+// router.get('/cleandb', async (req, res) => {
+// 	try {
+// 		await User.deleteMany();
+// 		res.status(200).json({
+// 			message: 'successfully cleaned db',
+// 		});
+// 	} catch (error) {
+// 		res.status(500).json({
+// 			message: 'db clean error',
+// 			error,
+// 		});
+// 	}
+// });
 
 router.post('/form', async (req, res) => {
 	const { fullName, sslcRegNo, mobile, school, batch, imageBinary } = req.body;
