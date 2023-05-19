4ed5f8bb7792197036e7aa831b624d2f9212c3de
shaheemMPM <shaheem.mpm@gmail.com>
2023-05-19 16:31:42 +0530

    Updates bucketname for sslc 2023

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,92 +1,92 @@
-import { Router } from 'express';
-import puppeteer from 'puppeteer';
-import fs from 'fs';
-import AWS from 'aws-sdk';
+import { Router } from "express";
+import puppeteer from "puppeteer";
+import fs from "fs";
+import AWS from "aws-sdk";
 
-import User from '../models/user';
+import User from "../models/user";
 
 const s3Bucket = new AWS.S3({
-	accessKeyId: process.env.AWS_ID,
-	secretAccessKey: process.env.AWS_SECRET,
-	region: 'ap-south-1',
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "ap-south-1",
 });
 
 const router = Router();
 
-const scrape = async ({ regNo, dob, mode = 'hse' }) => {
-	const browser = await puppeteer.launch({});
-	const page = await browser.newPage();
+const scrape = async ({ regNo, dob, mode = "hse" }) => {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
 
-	const dobParam = dob.split('/').join('%2F');
+  const dobParam = dob.split("/").join("%2F");
 
-	await page.goto(
-		`https://results.kite.kerala.gov.in/${mode}/result_schemeI.html?regno=${regNo}&date1=${dobParam}&Submit=Submit`
-	);
-	const studentData = await page.evaluate(() => {
-		const trimmedString = (str) => {
-			if (!str) return '';
-			return str.trim();
-		};
+  await page.goto(
+    `https://results.kite.kerala.gov.in/${mode}/result_schemeI.html?regno=${regNo}&date1=${dobParam}&Submit=Submit`
+  );
+  const studentData = await page.evaluate(() => {
+    const trimmedString = (str) => {
+      if (!str) return "";
+      return str.trim();
+    };
 
-		// get student name
-		const studentName = trimmedString(
-			document.getElementById('name').innerText.split(':')[1]
-		);
+    // get student name
+    const studentName = trimmedString(
+      document.getElementById("name").innerText.split(":")[1]
+    );
 
-		// get father's name
-		const fatherName = trimmedString(
-			document.getElementById('fname').innerText.split(':')[1]
-		);
+    // get father's name
+    const fatherName = trimmedString(
+      document.getElementById("fname").innerText.split(":")[1]
+    );
 
-		// get mother's name
-		const motherName = trimmedString(
-			document.getElementById('mname').innerText.split(':')[1]
-		);
+    // get mother's name
+    const motherName = trimmedString(
+      document.getElementById("mname").innerText.split(":")[1]
+    );
 
-		// get school code
-		const schoolCode = trimmedString(
-			document.getElementById('school').innerText.split(':')[1]
-		);
+    // get school code
+    const schoolCode = trimmedString(
+      document.getElementById("school").innerText.split(":")[1]
+    );
 
-		// get group name
-		const groupName = trimmedString(
-			document.getElementById('group_name').innerText.split(':')[1]
-		);
+    // get group name
+    const groupName = trimmedString(
+      document.getElementById("group_name").innerText.split(":")[1]
+    );
 
-		// get subject names
-		const subjectNameCells = document.getElementsByClassName('cell1');
-		const subjects = {};
-		for (let i = 0; i < subjectNameCells.length; i++) {
-			subjects[subjectNameCells[i].innerText] = document.getElementById(
-				`TP${i + 1}_TOT`
-			).innerText;
-		}
-		return {
-			studentName,
-			fatherName,
-			motherName,
-			schoolCode,
-			groupName,
-			subjects,
-		};
-	});
+    // get subject names
+    const subjectNameCells = document.getElementsByClassName("cell1");
+    const subjects = {};
+    for (let i = 0; i < subjectNameCells.length; i++) {
+      subjects[subjectNameCells[i].innerText] = document.getElementById(
+        `TP${i + 1}_TOT`
+      ).innerText;
+    }
+    return {
+      studentName,
+      fatherName,
+      motherName,
+      schoolCode,
+      groupName,
+      subjects,
+    };
+  });
 
-	const percentage =
-		(Object.values(studentData.subjects)
-			.map((val) => parseInt(val))
-			.reduce((partialSum, a) => partialSum + a, 0) *
-			100) /
-		1200;
+  const percentage =
+    (Object.values(studentData.subjects)
+      .map((val) => parseInt(val))
+      .reduce((partialSum, a) => partialSum + a, 0) *
+      100) /
+    1200;
 
-	browser.close();
+  browser.close();
 
-	return { ...studentData, percentage };
+  return { ...studentData, percentage };
 };
 
-router.get('/', async (req, res) => {
-	res.status(200).json({
-		test: 'test endpoint',
-	});
+router.get("/", async (req, res) => {
+  res.status(200).json({
+    test: "test endpoint",
+  });
 });
 
 // router.get('/results', async (req, res) => {
@@ -107,41 +107,41 @@ router.get('/', async (req, res) => {
 // 	return subjectString;
 // };
 
-router.get('/extract', async (req, res) => {
-	const users = await User.find();
+router.get("/extract", async (req, res) => {
+  const users = await User.find();
 
-	const newUsers = [];
-	for (let i = 0; i < users.length; i++) {
-		const user = users[i];
-		newUsers.push({
-			examRegNo: user.examRegNo,
-			studentName: user.studentName,
-			firstLanguage: user.firstLanguage,
-			english: user.english,
-			physics: user.physics,
-			chemistry: user.chemistry,
-			bioCs: user.bioCs,
-			maths: user.maths,
-			total: user.total,
-			mobile: user.mobile,
-			batch: user.batch,
-			photo: user.photo,
-			time: new Date(user.updatedAt).toLocaleString(),
-		});
-	}
+  const newUsers = [];
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    newUsers.push({
+      examRegNo: user.examRegNo,
+      studentName: user.studentName,
+      firstLanguage: user.firstLanguage,
+      english: user.english,
+      physics: user.physics,
+      chemistry: user.chemistry,
+      bioCs: user.bioCs,
+      maths: user.maths,
+      total: user.total,
+      mobile: user.mobile,
+      batch: user.batch,
+      photo: user.photo,
+      time: new Date(user.updatedAt).toLocaleString(),
+    });
+  }
 
-	fs.writeFile('users.json', JSON.stringify(newUsers), 'utf8', (err) => {
-		if (err) {
-			res.status(200).json({
-				message: 'failed',
-				err,
-			});
-		} else {
-			res.status(200).json({
-				message: 'success',
-			});
-		}
-	});
+  fs.writeFile("users.json", JSON.stringify(newUsers), "utf8", (err) => {
+    if (err) {
+      res.status(200).json({
+        message: "failed",
+        err,
+      });
+    } else {
+      res.status(200).json({
+        message: "success",
+      });
+    }
+  });
 });
 
 // router.get('/cleandb', async (req, res) => {
@@ -158,95 +158,95 @@ router.get('/extract', async (req, res) => {
 // 	}
 // });
 
-router.post('/form', async (req, res) => {
-	const {
-		examRegNo,
-		studentName,
-		firstLanguage,
-		english,
-		physics,
-		chemistry,
-		bioCs,
-		maths,
-		mobile,
-		batch,
-		imageBinary,
-	} = req.body;
+router.post("/form", async (req, res) => {
+  const {
+    examRegNo,
+    studentName,
+    firstLanguage,
+    english,
+    physics,
+    chemistry,
+    bioCs,
+    maths,
+    mobile,
+    batch,
+    imageBinary,
+  } = req.body;
 
-	const total =
-		Number(firstLanguage) +
-		Number(english) +
-		Number(physics) +
-		Number(chemistry) +
-		Number(bioCs) +
-		Number(maths);
+  const total =
+    Number(firstLanguage) +
+    Number(english) +
+    Number(physics) +
+    Number(chemistry) +
+    Number(bioCs) +
+    Number(maths);
 
-	console.log('Response came from: ', examRegNo);
+  console.log("Response came from: ", examRegNo);
 
-	const oldUser = await User.findOne({ examRegNo });
+  const oldUser = await User.findOne({ examRegNo });
 
-	const buf = Buffer.from(
-		imageBinary.replace(/^data:image\/\w+;base64,/, ''),
-		'base64'
-	);
-	const data = {
-		Bucket: 'insightprimes0',
-		Key: `photo/${examRegNo}.png`,
-		Body: buf,
-		ContentEncoding: 'base64',
-		ContentType: 'image/jpeg',
-	};
+  const buf = Buffer.from(
+    imageBinary.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  );
+  const data = {
+    Bucket: "digitalcampaing",
+    Key: `photo/${examRegNo}.png`,
+    Body: buf,
+    ContentEncoding: "base64",
+    ContentType: "image/jpeg",
+  };
 
-	try {
-		await s3Bucket.upload(data).promise();
-	} catch (error) {
-		res.status(500).json({
-			message: 'error in s3 upload',
-			error,
-		});
-	}
+  try {
+    await s3Bucket.upload(data).promise();
+  } catch (error) {
+    res.status(500).json({
+      message: "error in s3 upload",
+      error,
+    });
+  }
 
-	if (oldUser) {
-		await User.findByIdAndUpdate(
-			oldUser.id,
-			{
-				$set: {
-					examRegNo,
-					studentName,
-					firstLanguage,
-					english,
-					physics,
-					chemistry,
-					bioCs,
-					maths,
-					total,
-					mobile,
-					batch,
-					photo: `https://insightprimes0.s3.ap-south-1.amazonaws.com/photo/${examRegNo}.png`,
-				},
-			},
-			{ new: true }
-		);
-	} else {
-		await new User({
-			examRegNo,
-			studentName,
-			firstLanguage,
-			english,
-			physics,
-			chemistry,
-			bioCs,
-			maths,
-			total,
-			mobile,
-			batch,
-			photo: `https://insightprimes0.s3.ap-south-1.amazonaws.com/photo/${examRegNo}.png`,
-		}).save();
-	}
+  if (oldUser) {
+    await User.findByIdAndUpdate(
+      oldUser.id,
+      {
+        $set: {
+          examRegNo,
+          studentName,
+          firstLanguage,
+          english,
+          physics,
+          chemistry,
+          bioCs,
+          maths,
+          total,
+          mobile,
+          batch,
+          photo: `https://insightprimes0.s3.ap-south-1.amazonaws.com/photo/${examRegNo}.png`,
+        },
+      },
+      { new: true }
+    );
+  } else {
+    await new User({
+      examRegNo,
+      studentName,
+      firstLanguage,
+      english,
+      physics,
+      chemistry,
+      bioCs,
+      maths,
+      total,
+      mobile,
+      batch,
+      photo: `https://insightprimes0.s3.ap-south-1.amazonaws.com/photo/${examRegNo}.png`,
+    }).save();
+  }
 
-	res.status(200).json({
-		message: 'form created/updated',
-	});
+  res.status(200).json({
+    message: "form created/updated",
+  });
 });
 
 export default router;
