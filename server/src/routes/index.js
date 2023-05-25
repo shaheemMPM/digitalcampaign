47d663d208d0946ff0179478531adeac8980c43a
shaheemMPM <shaheem.mpm@gmail.com>
2023-05-25 18:31:37 +0530

    Updates photo url bucket name

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -216,7 +216,7 @@ router.post('/form', async (req, res) => {
 			percentage,
 			mobile,
 			batch,
-			photo: `https://insightprimes0.s3.ap-south-1.amazonaws.com/photo/${examRegNo}.png`,
+			photo: `https://digitalcampaing.s3.ap-south-1.amazonaws.com/photo/${examRegNo}.png`,
 		}).save();
 	}
 
